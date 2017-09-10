8d45e1b7b0b724308fe821524d7d17eff7e70965
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2017-09-10 20:10:56 -0300

    permitir compilação do impdbf

--- sources/nongui/main.cpp
+++ sources/nongui/main.cpp
@@ -19,8 +19,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "ntable.h"
 #include "xbtable.h"
 
-
 using namespace std;
+
+bool convertOK;
 
 void
 import_dbf(wxString name)
